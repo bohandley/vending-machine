f8a3b891b7a3df7fd737632e724818fe44e742ff
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-11-18 13:53:43 -0600

    refactor machine to remove float values and replace with integers. Refactor makeChange method.

--- machine.js
+++ machine.js
@@ -1,7 +1,7 @@
 var Machine = function(args) {
 	this.totalCoins = args.totalCoins || [];
 	this.insertedCoins = [];
-	this.currentAmount = 0.00.toFixed(2);
+	this.currentAmount = 0;
 	this.coinReturn = [];
 	this.productReturn = [];
 	this.inventory = args.inventory || [];
@@ -9,9 +9,9 @@ var Machine = function(args) {
 };
 
 var weights = { 
-	630: 0.05,
-	415: 0.10,
-	772: 0.25
+	630: 5,
+	415: 10,
+	772: 25
 };
 
 var displays = [
@@ -23,9 +23,9 @@ var displays = [
 ];
 
 var monies = [
-	0.25,
-	0.10,
-	0.05
+	25,
+	10,
+	5
 ];
 
 // Machine rejects pennies but accepts nickels, dimes and quarters
@@ -41,13 +41,13 @@ Machine.prototype.insertCoins = function(coin) {
 Machine.prototype.loadCoins = function(coins) {
 	this.totalCoins = coins.map(function(coin){
 		coin.value = weights[coin.weight];
-		coin.value.toFixed(2);
+		coin.value;
 		return coin;
 	});
-	this.setDisplay();
+	this.setInitialDisplay();
 };
 
-Machine.prototype.setDisplay = function(){
+Machine.prototype.setInitialDisplay = function(){
 	this.display = this.initialDisplay();
 };
 
@@ -85,11 +85,11 @@ Machine.prototype.setDisplayToSoldOut = function(){
 };
 
 Machine.prototype.displayProductPrice = function(product){
-	this.display = displays[1] + product.price.toFixed(2);
+	this.display = displays[1] + product.price;
 };
 
 Machine.prototype.resetCurrentAmountDisplay = function(){
-	this.currentAmount = 0.00.toFixed(2);
+	this.currentAmount = 0;
 };
 
 Machine.prototype.addInsertedCoinsToTotalCoins = function(){
@@ -105,7 +105,7 @@ Machine.prototype.getProductFromInventory = function(product){
 	var index = this.inventory.findIndex(function(element){
 		return element.name == product.name;
 	});
-	switch(index) {
+	switch( index ) {
 		case -1 :
 			return false;
 		default :		
@@ -113,42 +113,30 @@ Machine.prototype.getProductFromInventory = function(product){
 	}
 };
 
-// Sum inserted coins, set value attribute of each coin, set currentAmount display
+// Iterate through inserted coins, set value attribute of each coin, 
+// sum values, set currentAmount display
 Machine.prototype.sumInsertedCoins = function() {
-	// Create an array of inserted coins
-	var total = this.insertedCoins.map(function(x){
-		// Assign a value to a coin when it is inserted
+	this.currentAmount = this.insertedCoins.map(function(x){
 		x.value = weights[x.weight];
 		return x.value;
-	});
-	// If the array is not empty, sum the array
-	if ( total.length != 0 ) {
-		var sum = total.reduce(function(a,b) {
+	}).reduce(function(a,b) {
 			return a + b;
-		});
-		sum = sum.toFixed(2);
-		// Set the currentAmount display to the amount of money inserted
-		this.currentAmount = sum;
-	} else {
-		// Display 0.00 if nothing has been inserted
-		this.currentAmount = 0.00.toFixed(2);
-	}
+	},0);
 };
 
-// When a product is purchased that costs less than the amount inserted, the machine makes change
 Machine.prototype.makeChange = function(product) {
 	// Identify how much change needs to be made
 	var valueReturned = this.currentAmount - product.price;
-	valueReturned = valueReturned.toFixed(2);
+	this.makeChangeAlgorithm(valueReturned);
+};
+
+// Make change by looping through the monies array. For each value, 25, 10, and 5,
+// divide the valueReturned by the value of the coin(element) of the monies array. 
+Machine.prototype.makeChangeAlgorithm = function(valueReturned){
 	var machine = this;
-	// Make change by looping through the monies array. For each value, 25, 10, and 5,
-	// divide the valueReturned by the value of the coin(element) of the monies array. 
-	monies.forEach(function(coin){
+	monies.map(function(coin){
 		var times =  valueReturned / coin;
-		if ( times > 0.99 && times < 1 ) {
-			times = 1;
-		}
-		times = Math.floor(times);
+		times = Math.floor(times)
 		var i=0;
 		for (i; i < times; i++) {
 			var index = this.totalCoins.findIndex(function(element){
@@ -158,46 +146,40 @@ Machine.prototype.makeChange = function(product) {
 		}
 		valueReturned = valueReturned - (times * coin);
 	},machine);
-};
+}
 
 // When a customer changes their mind before choosing a product, they can have their money returned
 Machine.prototype.pressCoinReturn = function() {
 	this.coinReturn = this.coinReturn.concat(this.insertedCoins);
 	this.insertedCoins = new Array;
-	this.currentAmount = 0.00.toFixed(2);
+	this.resetCurrentAmountDisplay();
 };
 
 // Take a product that has been paid for from the product return
 Machine.prototype.removeProduct = function() {
 	this.display = this.initialDisplay();
 	this.productReturn = new Array;
-	this.currentAmount = 0.00.toFixed(2);
+	this.resetCurrentAmountDisplay();
 };
 
 // Remove the coins from the coin return
 Machine.prototype.takeCoins = function(){
 	this.coinReturn = new Array;
-	this.currentAmount = 0.00.toFixed(2);
+	this.resetCurrentAmountDisplay();
 };
 
-// Sum the total amount of coins inside the machine
+// Sum the total amount of coins inside the machine 
 Machine.prototype.sumTotalCoins = function() {
-	if ( this.totalCoins.length != 0 ) {
-		var valueAmount = this.totalCoins.map(function(element){
+		return this.totalCoins.map(function(element){
 			return element.value;
-		});
-		var total = valueAmount.reduce(function(a,b){
+		}).reduce(function(a,b){
 			return a + b;
 		},0);
-		return total;
-	} else {
-		[];
-	}
 };
 
 // Set the initial display of the machine to INSERT COIN or EXACT CHANGE
 Machine.prototype.initialDisplay = function() {
-	if ( this.sumTotalCoins() >= 1 ) {
+	if ( this.sumTotalCoins() >= 100 ) {
 		return displays[0];
 	} else {
 		return displays[3];
